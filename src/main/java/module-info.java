c02764773a2dd81be3375e1fb9e012208aabef39
alexandredermouche <alexandre.dermouche@gmail.com>
2024-02-12 15:23:18 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.openjfx.bioreactor {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.openjfx.bioreactor to javafx.fxml;
+    exports org.openjfx.bioreactor;
+}
