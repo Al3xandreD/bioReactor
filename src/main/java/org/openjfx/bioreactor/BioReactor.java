ea356e9f719f0403ba45f7c2f331247d38405408
alexandredermouche <alexandre.dermouche@gmail.com>
2024-02-19 13:50:16 +0100

    update

--- src/main/java/org/openjfx/bioreactor/BioReactor.java
+++ src/main/java/org/openjfx/bioreactor/BioReactor.java
@@ -1,30 +1,56 @@
 package org.openjfx.bioreactor;
 
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.poi.ss.usermodel.*;
 import java.io.File;
 
+/** Représente un jumeau numérique de bio réacteur */
 public class BioReactor {
     private ArrayList<ServeurTCP> serveurTCPS=new ArrayList<>();
+
+    /** température mesurée*/
     private float tempMes=0;
+
+    /** température en consigne*/
     private float tempCons=0;
+
+    /** température en commande*/
     private float tempComm=0;
+
+    /** oxygène mesurée */
     private float oxyMes=0;
+
+    /** ph mesurée*/
     private float phMes=0;
 
-    public BioReactor(ArrayList<ServeurTCP> serveurTCPS, float tempMes, float tempCons, float tempComm, float oxyMes, float phMes) {
-        this.serveurTCPS = serveurTCPS;
-        this.tempMes = tempMes;
-        this.tempCons = tempCons;
-        this.tempComm = tempComm;
-        this.oxyMes = oxyMes;
-        this.phMes = phMes;
+    public BioReactor() {
+
+        try {
+            String excelFilePath = "/Users/alexandredermouche/Documents/Alexandre /Cours/ENSTA/2A/STIC/conception logicielle/bioReactor/src/main/java/2022-10-03-Act2-1.xlsx";
+            InputStream inputStream = new FileInputStream(excelFilePath);
+
+            Workbook myWb = new XSSFWorkbook(inputStream);  //new workbook
+            Sheet firstSheet = myWb.getSheetAt(0);  //only sheet
+
+            // retrieving data
+            this.tempMes = Float.parseFloat(firstSheet.getRow(1).getCell(7).getStringCellValue());
+
+            //this.tempCons = tempCons;
+//        this.tempComm = tempComm;
+//        this.oxyMes = oxyMes;
+//        this.phMes = phMes;
+            System.out.println(this.tempMes);
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
     }
 
-    public void initReactor(FileInputStream path){
-        Workbook myWb = new WorkbookFactory.create(path);
-        Sheet firstSheet=myWb.getSheetAt(0);
-        this.tempMes=firstSheet.getN
+    public void initReactor(){
     }
 }
--- src/main/java/packageClient/ServeurClient.java
+++ src/main/java/packageClient/ServeurClient.java
@@ -1,4 +1,62 @@
 package packageClient;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ConnectException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/** Représente un serveur TCP client*/
 public class ServeurClient {
+
+    int numeroPort;
+    private String nomServeur;
+
+    private Socket socketServeur;
+
+    private PrintStream socOut;
+
+    private BufferedReader socIn;
+
+    public ServeurClient(String nomServeur, int numeroPort){
+        this.nomServeur=nomServeur;
+        this.numeroPort=numeroPort;
+    }
+
+    public boolean connecterAuServeur() {
+        boolean ok = false;
+        try {
+            System.out.println("Tentative : " + nomServeur + " -- " + numeroPort);
+            socketServeur = new Socket(nomServeur, numeroPort);
+            socOut = new PrintStream(socketServeur.getOutputStream());
+            socIn = new BufferedReader(new InputStreamReader(socketServeur.getInputStream()));
+            ok = true;
+        } catch (UnknownHostException e) {
+            System.err.println("Serveur inconnu : " + e);
+
+        } catch (ConnectException e) {
+            System.err.println("Exception lors de la connexion:" + e);
+            e.printStackTrace();
+
+        } catch (IOException e) {
+            System.err.println("Exception lors de l'echange de donnees:" + e);
+        }
+        System.out.println("Connexion faite");
+        return ok;
+    }
+
+    public void deconnecterDuServeur() {
+        try {
+            System.out.println("[ClientTCP] CLIENT : " + socketServeur);
+            socOut.close();
+            socIn.close();
+            socketServeur.close();
+        } catch (Exception e) {
+            System.err.println("Exception lors de la deconnexion :  " + e);
+        }
+    }
+
+
 }
