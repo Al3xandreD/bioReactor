bdb8f232950d0f8574901662bf2dd43cd13dfedf
rapha <raphael.grisel@ensta-bretagne.org>
2024-02-26 14:04:36 +0100

    ServeurTCP modifié

--- src/main/java/org/openjfx/bioreactor/ServeurTCP.java
+++ src/main/java/org/openjfx/bioreactor/ServeurTCP.java
@@ -1,71 +1,58 @@
-package org.openjfx.bioreactor;
-
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-
 /**
- * Représente un serveur TCP, qui écoute sur un numéro de port
+ * ServeurTCP est une classe qui gère le serveur TCP.
+ * Elle possède une méthode startServer qui écoute en permanence les connexions entrantes et traite
+ * les données à l'aide de la classe Bioreacteur. La méthode main dans
+ * TCPServer est utilisée pour démarrer le serveur sur le port spécifié.
  *
- */
+ *
+ * */
 
-public class ServeurTCP extends Thread {
 
-    private static int nbConnexions = 0;
+import org.openjfx.bioreactor.BioReactor;
 
-    /** Maximum de connexions client autorisées */
-    private int maxConnexions;
+import java.io.*;
+import java.net.*;
 
-    /** Socket du serveur client */
-    private Socket clientSocket;
+public class ServeurTCP {
 
-    /** Numéro de port d'ecoute */
-    private int numeroPort;
+    private ServerSocket serverSocket;
 
-    public ServeurTCP(int numeroPort, int maxConnexions){
-        this.numeroPort=numeroPort;
-        this.maxConnexions=maxConnexions;
-    }
-
-    @Override
-    public String toString(){
-        return "[ServeurTCP] Port : " + numeroPort+" Nombre de connexions"+nbConnexions+" Connecté au client"+clientSocket;
-    }
-
-    @Override
-    public void run() {
-
-        // initialisation
-        ServerSocket serverSocket=null; //necessaire car si fail initialisation, pas de serveurSocket pour la while
+    public ServeurTCP(int port) {
         try {
-            serverSocket = new ServerSocket(numeroPort);
-        }
-        catch (IOException e) {
-            System.out.println("Could not listen on port: " + numeroPort + ", " + e);
-            System.exit(1);
-        }
-
-        // autorisation des connexions
-        while (nbConnexions <= maxConnexions) {
-            try {
-                System.out.println(" Attente du serveur pour la communication d'un client ");
-                clientSocket = serverSocket.accept();
-                nbConnexions++;
-                System.out.println("Nb automates : " + nbConnexions);
-            }
-            catch (IOException e) {
-                System.out.println("Accept failed: " + serverSocket.getLocalPort() + ", " + e);
-                System.exit(1);
-            }
-        }
-        System.out.println("Deja " + nbConnexions + " clients. Maximum autorisé atteint");
-
-        // fermeture du serveur
-        try {
-            serverSocket.close();
-            nbConnexions--;
+            serverSocket = new ServerSocket(port);
+            System.out.println("Serveur en attente de connexions sur le port " + port);
         } catch (IOException e) {
-            System.out.println("Could not close");
+            e.printStackTrace();
         }
+    }
+
+    public void startServer() {
+        try {
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Client connecté depuis " + clientSocket.getInetAddress());
+
+                // Créer une instance de la classe de traitement des données
+                BioReactor bioreacteur = new BioReactor();
+
+                // Lire les données avec la classe de traitement des données
+                String data = BioReactor.readingFile();
+
+                // Envoyer les données au client
+                PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
+                writer.println(data);
+
+                // Fermer les flux et la connexion avec le client
+                writer.close();
+                clientSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        ServeurTCP tcpServer = new ServeurTCP(12345);
+        tcpServer.startServer();
     }
 }
